3becb4beec40b0023652c9a7f03921c1200a640e
Anna Tsepilova <virgo.tsav@gmail.com>
2024-05-02 17:54:57 +0300

    add functions

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,4 +1,9 @@
-import contactsService from "../services/contactsServices.js";
+import {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+} from "../services/contactsServices.js";
 
 export const getAllContacts = (req, res) => {};
 
